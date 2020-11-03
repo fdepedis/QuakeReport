5964c37c92d7c3771484f192033ad425eb27a9a8
Flavio <flavio.depedis@gmail.com>
2020-11-03 21:49:33 +0100

    review setting for notification

--- app/src/main/java/it/fdepedis/quakereport/utils/NotificationUtils.java
+++ app/src/main/java/it/fdepedis/quakereport/utils/NotificationUtils.java
@@ -1,25 +1,21 @@
 package it.fdepedis.quakereport.utils;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.GradientDrawable;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
-
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import it.fdepedis.quakereport.R;
-import it.fdepedis.quakereport.sync.QuakeReportSyncTask;
 
 public class NotificationUtils {
 
@@ -36,7 +32,7 @@ public class NotificationUtils {
 
         Resources resources = context.getResources();
         int largeArtResourceId = R.mipmap.ic_launcher;
-        int smallArtResourceId = R.drawable.ic_notification;
+        int smallArtResourceId = R.drawable.ic_stat_album;
 
         Bitmap largeIcon = BitmapFactory.decodeResource(
                 resources,
@@ -45,13 +41,6 @@ public class NotificationUtils {
         String notificationTitle = context.getString(R.string.app_name);
         String notificationText = place;
 
-        /*
-         * NotificationCompat Builder is a very convenient way to build backward-compatible
-         * notifications. In order to use it, we provide a context and specify a color for the
-         * notification, a couple of different icons, the title for the notification, and
-         * finally the text of the notification, which in our case in a summary of today's
-         * forecast.
-         */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
             Log.e(LOG_TAG, "notifications???");
@@ -69,7 +58,6 @@ public class NotificationUtils {
             Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
 
             // Create an explicit intent for an Activity in your app
-            //Intent intent = new Intent(this, AlertDetails.class);
             websiteIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, websiteIntent, 0);
 
@@ -79,17 +67,14 @@ public class NotificationUtils {
                     .setLargeIcon(largeIcon)
                     .setContentTitle(notificationTitle)
                     .setContentText(notificationText)
+                    .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 }) //Vibration
+                    .setLights(Color.RED, 3000, 3000) //LED
                     .setContentIntent(pendingIntent)
                     .setAutoCancel(true)
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-            /*
-             * This Intent will be triggered when the user clicks the notification. In our case,
-             * we want to open Sunshine to the DetailActivity to display the newly updated weather.
-             */
             /*Intent detailIntentForToday = new Intent(context, DetailActivity.class);
             detailIntentForToday.setData(todaysWeatherUri);*/
-
 
             /*
             TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
@@ -100,39 +85,17 @@ public class NotificationUtils {
             notificationBuilder.setContentIntent(resultPendingIntent);
             */
 
-            /*NotificationManager notificationManager = (NotificationManager)
-                    context.getSystemService(Context.NOTIFICATION_SERVICE);*/
-
             NotificationManagerCompat notification = NotificationManagerCompat.from(context);
 
-            /* WEATHER_NOTIFICATION_ID allows you to update or cancel the notification later on */
             notification.notify(QUAKE_REPORT_NOTIFICATION_ID, notificationBuilder.build());
 
             /*
              * Since we just showed a notification, save the current time. That way, we can check
              * next time the weather is refreshed if we should show another notification.
              */
-            //SunshinePreferences.saveLastNotificationTime(context, System.currentTimeMillis());
+            //QuakeReportPreferences.saveLastNotificationTime(context, System.currentTimeMillis());
         }
 
-
-        /* Always close your cursor when you're done with it to avoid wasting resources. */
-        //todayWeatherCursor.close();
     }
 
-    //private static String getNotificationText(Context context, int weatherId, double high, double low) {
-
-        /*String shortDescription = SunshineWeatherUtils
-                .getStringForWeatherCondition(context, weatherId);
-
-        String notificationFormat = context.getString(R.string.format_notification);*/
-
-        /* Using String's format method, we create the forecast summary */
-        /*String notificationText = String.format(notificationFormat,
-                shortDescription,
-                SunshineWeatherUtils.formatTemperature(context, high),
-                SunshineWeatherUtils.formatTemperature(context, low));
-
-        return notificationText;*/
-    //}
 }
