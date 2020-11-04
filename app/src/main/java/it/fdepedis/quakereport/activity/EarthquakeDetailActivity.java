5811109fb9e1d95125a809f5f4f7b6135428d22c
Flavio <flavio.depedis@gmail.com>
2020-11-04 16:29:11 +0100

    EarthquakeDetailActivity

---
+++ app/src/main/java/it/fdepedis/quakereport/activity/EarthquakeDetailActivity.java
@@ -0,0 +1,15 @@
+package it.fdepedis.quakereport.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import it.fdepedis.quakereport.R;
+
+import android.os.Bundle;
+
+public class EarthquakeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earthquake_detail);
+    }
+}
